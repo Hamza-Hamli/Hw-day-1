3e62f2d602882f92a62cedda82ef3ee57bf631b9
Hamza-Hamli <hamzah.alhamli@gmail.com>
2022-09-16 23:03:52 +0300

    doing HW part 2

--- JS/script.js
+++ JS/script.js
@@ -8,5 +8,21 @@ let z=(x+y);
 console.log(y);
 
 
+//convert Celsius to Fahrenheit
+
+let C=(25);
+let F=(C+33.8);
+
+console.log(F);
+
+// convert Fahrenheit to Celsius
+
+
+let f=(98);
+let c=(f-33.8);
+
+console.log(c);
+
+
 
 
